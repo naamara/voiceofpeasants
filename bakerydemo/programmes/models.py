92e9e4fc23a483c0c2066fe9f115798702461a97
naamarahannz <hannnington.naamara@kipya-connect.com>
2017-10-26 12:25:19 +0300

    changes on the programmes page

--- bakerydemo/programmes/models.py
+++ bakerydemo/programmes/models.py
@@ -141,23 +141,11 @@ class ProgrammePage(Page):
     body = StreamField(
         BaseStreamBlock(), verbose_name="Page body", blank=True
     )
-    address = models.TextField()
-    lat_long = models.CharField(
-        max_length=36,
-        help_text="Comma separated lat/long. (Ex. 64.144367, -21.939182) \
-                   Right click Google Maps and select 'What\'s Here'",
-        validators=[
-            RegexValidator(
-                regex='^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$',
-                message='Lat Long must be a comma-separated numeric lat and long',
-                code='invalid_lat_long'
-            ),
-        ]
-    )
+   
 
     # Search index configuration
     search_fields = Page.search_fields + [
-        index.SearchField('address'),
+       
         index.SearchField('body'),
     ]
 
@@ -167,41 +155,19 @@ class ProgrammePage(Page):
         FieldPanel('introduction', classname="full"),
         ImageChooserPanel('image'),
         StreamFieldPanel('body'),
-        FieldPanel('address', classname="full"),
-        FieldPanel('lat_long'),
-        InlinePanel('hours_of_operation', label="Hours of Operation"),
+     
     ]
 
     def __str__(self):
         return self.title
 
-    @property
-    def operating_hours(self):
-        hours = self.hours_of_operation.all()
-        return hours
-
-    # Determines if the location is currently open. It is timezone naive
-    def is_open(self):
-        now = datetime.now()
-        current_time = now.time()
-        current_day = now.strftime('%a').upper()
-        try:
-            self.operating_hours.get(
-                day=current_day,
-                opening_time__lte=current_time,
-                closing_time__gte=current_time
-            )
-            return True
-        except ProgrammeOperatingHours.DoesNotExist:
-            return False
+  
 
     # Makes additional context available to the template so that we can access
     # the latitude, longitude and map API key to render the map
     def get_context(self, request):
         context = super(ProgrammePage, self).get_context(request)
-        context['lat'] = self.lat_long.split(",")[0]
-        context['long'] = self.lat_long.split(",")[1]
-        context['google_map_api_key'] = settings.GOOGLE_MAP_API_KEY
+      
         return context
 
     # Can only be placed under a LocationsIndexPage object
